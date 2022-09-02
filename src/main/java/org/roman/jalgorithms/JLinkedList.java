a61261967f597e699da7f3d4ab0e9e8748bc06d3
Roman <sh-roman@outlook.com>
2022-09-02 20:26:27 +0500

    project improvement

--- src/main/java/org/roman/jalgorithms/JLinkedList.java
+++ src/main/java/org/roman/jalgorithms/JLinkedList.java
@@ -5,43 +5,110 @@ import java.util.LinkedList;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
-public class JLinkedList<Value> {
+public class JLinkedList<Value extends Comparable<Value>> {
 
-    Node<Value> head = null;
-    Node<Value> tail = null;
+    private Node<Value> head = null;
+    private Node<Value> tail = null;
 
-
-    int size = 0;
-
+    private int size = 0;
 
     private static class Node<T> {
-        private T value;
-        private Node<T> next;
+        private T value = null;
+        private Node<T> next = null;
+
+        public Node(T value) {
+            this.value = value;
+        }
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
     }
 
 
+    //Удалить первое вхождение объекта
+    public boolean remove(Value value) {
+        Node<Value> search = head;
+        Node<Value> previousNode = null;
+        while (search != null) {
+            //Элемент найден, произвести удаление
+            if(value.equals(search.value)) {
+                if (head == tail) {
+                    head = tail = null;
+                } else {
+                    //Объектов 2 или больше
+                    if (previousNode == null)
+                        head = search.next;
+                    else if(search.next == null) {
+                        previousNode.next = null;
+                        tail = previousNode;
+                    }
+                    else {
+                        previousNode.next = search.next;
+                    }
+                }
+                --size;
+                return true;
+            }
+            //Сохраняем ссылку на предыдущую ноду
+            previousNode = search;
+            search = search.next;
+        }
+        return false;
+    }
 
+    public boolean contains(Value value) {
+        Node<Value> search = head;
+        while (search != null) {
+            if(value.equals(search.value))
+                return true;
+            search = search.next;
+        }
+        return false;
+    }
+
+    public int indexOf(Value value) {
+        Node<Value> search = head;
+        int i = 0;
+        while (search != null) {
+            if(value.equals(search.value)) return i;
+            search = search.next;
+            ++i;
+        }
+        return -1;
+    }
+
+    public Value get(int index) {
+        Node<Value> search = head;
+        int i = 0;
+        while (search != null) {
+            if(index == i) return search.value;
+            search = search.next;
+            ++i;
+        }
+        return null;
+    }
 
 
 
     //Добавить в конец
     public void add(Value value) {
-        //
-
-        Node<Value> temp = new Node<>();
-        temp.value = value;
-
+        Node<Value> temp = new Node<>(value);
+        //Если лист пустой, то начало и конец списка указываем на новый элемент
         if(head == null) {
             head = temp;
-            return;
+            tail = temp;
+        } else {
+            tail.next = temp;
+            tail = temp;
         }
-
-
-
-        //
-
         size++;
     }
 
+    public int size() {
+        return size;
+    }
 
 }
--- src/main/java/org/roman/jalgorithms/Main.java
+++ src/main/java/org/roman/jalgorithms/Main.java
@@ -4,11 +4,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException {
 
 
         //
+        //ArrayList<Integer> q;
+        //q.get();
 
+
+
+        JLinkedList<Integer> list = new JLinkedList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        System.out.println(list.size());
+
+        //list.remove(0);
+        //list.remove(9);
+
+        System.out.println(list.size());
+        for (int i = 0; i < 10; i++) {
+            System.out.println(list.contains(i));
+        }
+
+        System.out.println(list.get(9));
 
 
 
